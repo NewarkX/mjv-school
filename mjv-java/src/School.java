ca55d84daf4f1c9b237b6d7bfe1775f676158cdf
NewarkX <brunowmanha@gmail.com>
2022-02-24 20:42:21 -0300

    exemplo de funcao lambda

---
+++ mjv-java/src/School.java
@@ -0,0 +1,18 @@
+public class School {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.name = "bruno";
+        st.age = 29;
+        st.sex = 'M';
+        st.color = "White";
+        System.out.println(st.name);
+
+        Student st2 = new Student();
+        st2.name = "isabella";
+        st2.age = 25;
+        st2.sex = 'F';
+        st2.color = "White";
+        System.out.println(st2.name);
+
+    }
+}
