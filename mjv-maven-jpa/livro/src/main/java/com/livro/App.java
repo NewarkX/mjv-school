3489dd5a7d8479ef98955e3e37b213ecaf1e100b
NewarkX <brunowmanha@gmail.com>
2022-03-15 12:39:51 -0300

    create,findOne and findAll

--- mjv-maven-jpa/livro/src/main/java/com/livro/App.java
+++ mjv-maven-jpa/livro/src/main/java/com/livro/App.java
@@ -1,6 +1,9 @@
 package com.livro;
 
+import com.livro.model.Livro;
 import com.livro.repositories.LivroRepository;
+
+import java.util.List;
 
 /**
  * Hello world!
@@ -11,6 +14,21 @@ public class App
     public static void main( String[] args )
     {
         LivroRepository livroRepository = new LivroRepository();
+//      Livro livro = new Livro();
+//      livro.setNome("A bela e a fera");
+//      livro.setAutor("Gabrielle-Suzanne Barbot");
+//      livro.setEdicao("1");
+//      livroRepository.save(livro);
 
+//        Livro livro = livroRepository.findById(1);
+//        if(livro != null){
+//            System.out.println(livro.getNome());
+//            System.out.println(livro);
+//        }
+
+        List<Livro> livros = livroRepository.findAll();
+        for (Livro l: livros){
+            System.out.println(l);
+        }
     }
 }
